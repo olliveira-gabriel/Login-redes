8a0bf5bbac37e50127a89b49026956a6c863362a
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-09-11 17:26:27 -0300

    Add files via upload

---
+++ login-redes-main/desafio-extra/desafio-extra.js
@@ -0,0 +1,15 @@
+
+
+const login = JSON.parse(localStorage.getItem('login'));
+console.log(login);
+
+var json_Name = window.localStorage.getItem('Name');
+
+var Name = JSON.parse(json_Name);
+
+console.log(Name)
+
+
+
+
+
